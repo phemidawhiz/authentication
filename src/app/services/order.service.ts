6a9eac868c34030eef242a9d88e929b2c877668a
phemidawhiz <feminiscence@gmail.com>
2020-05-26 11:33:28 +0100

    error handling and seperation of concern

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,34 +1,13 @@
+import { DataService } from './data.service';
 import { AuthHttp } from 'angular2-jwt';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class OrderService {
+export class OrderService extends DataService {
 
-  // Angular shortcut
-  constructor(private authHttp: AuthHttp) {
+  constructor(authHttp: AuthHttp) {
+    super('http://localhost:3000/api/users/', authHttp);
   }
-
-  getUsers() {
-    return this.authHttp.get('http://localhost:3000/api/users/')
-      .map(response => {
-        response.json();
-      });
-  }
-
-  // What's happening under the hood
-  /* constructor(private http: Http) {
-  }
-
-  getUsers() {
-    const headers = new Headers();
-    headers.append('Authorisation', 'Bearer' + localStorage.getItem('token'));
-
-    const options = new RequestOptions({ headers: headers });
-    return this.http.get('http://localhost:3000/api/users/', options)
-      .map(response => {
-        response.json();
-      });
-  } */
 }
